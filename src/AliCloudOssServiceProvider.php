9a13e4f62051fe89eef02080ae5b6552ca379c27
卢艺源 <sebastiankennedy@foxmail.com>
2018-09-10 11:38:14 +0800

    add laravel service provider

--- src/AliCloudOssServiceProvider.php
+++ src/AliCloudOssServiceProvider.php
@@ -1,7 +1,41 @@
 <?php
 
-namespace SebastianKennedy\LaravelFlySystemAliCloudOss;
+namespace App\Providers;
 
-class AliCloudOssServiceProvider
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\ServiceProvider;
+use League\Flysystem\Filesystem;
+use SebastianKennedy\LaravelFlySystemAliCloudOss\AliCloudOssAdapter;
+
+class AliCloudOssServiceProvider extends ServiceProvider
 {
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Storage::extend('ali_cloud_oss', function ($app, $config) {
+            $accessKeyId = $config['access_key_id'];
+            $accessKeySecret = $config['access_key_secret'];
+            $endpoint = $config['endpoint'];
+            $bucket = $config['bucket'];
+
+            $adapter = new AliCloudOssAdapter($accessKeyId, $accessKeySecret, $endpoint, $bucket);
+            $fileSystem = new Filesystem($adapter);
+
+            return $fileSystem;
+        });
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
 }
